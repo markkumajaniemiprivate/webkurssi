bbce25515771289e26e6072faebb0a5294d95efe
Markku Majaniemi <markku.majaniemi@gmail.com>
2020-04-03 16:16:31 +0300

    unicafe step6 fix

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -14,30 +14,32 @@ const Statistics = ({ good, neutral, bad }) => {
     <div>
       <h1>Statistics</h1>
       <table>
-        <tr>
-          <td>good</td>
-          <td> {good}</td>
-        </tr>
-        <tr>
-          <td>neutral</td>
-          <td> {neutral}</td>
-        </tr>
-        <tr>
-          <td>bad</td>
-          <td> {bad}</td>
-        </tr>
-        <tr>
-          <td>all</td>
-          <td> {bad + neutral + good}</td>
-        </tr>
-        <tr>
-          <td>average</td>
-          <td> {(good - bad) / (bad + neutral + good)}</td>
-        </tr>
-        <tr>
-          <td>positive</td>
-          <td> {(good) / (bad + neutral + good) * 100} %</td>
-        </tr>
+        <tbody>
+          <tr>
+            <td>good</td>
+            <td> {good}</td>
+          </tr>
+          <tr>
+            <td>neutral</td>
+            <td> {neutral}</td>
+          </tr>
+          <tr>
+            <td>bad</td>
+            <td> {bad}</td>
+          </tr>
+          <tr>
+            <td>all</td>
+            <td> {bad + neutral + good}</td>
+          </tr>
+          <tr>
+            <td>average</td>
+            <td> {(good - bad) / (bad + neutral + good)}</td>
+          </tr>
+          <tr>
+            <td>positive</td>
+            <td> {(good) / (bad + neutral + good) * 100} %</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   )
@@ -56,18 +58,6 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const handleGoodClick = () => {
-    setGood(good + 1)
-  }
-
-  const handleNeutralClick = () => {
-    setNeutral(neutral + 1)
-  }
-
-  const handleBadClick = () => {
-    setBad(bad + 1)
-  }
 
   return (
     <div>
